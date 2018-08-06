c46327fc4f88c9dee62309815de2a72690d3a3fd
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-08-06 22:49:08 +0900

    UPDATE // member/signup
    signup completion

--- Seoul_Busking_API/routes/member/signup.js
+++ Seoul_Busking_API/routes/member/signup.js
@@ -1,7 +1,7 @@
 /*
 	URL : /member/signup
 	Description : 회원가입
-	Content-type : form_data
+	Content-type : x-www-form-urlencoded
 	method : POST - Body
 	Body = {
 		member_type : String ,			/	일반인 : 0 , 공연자 : 1
@@ -9,9 +9,7 @@
 		member_ID : String , 			/	아이디
 		member_PW : String ,			/	비밀번호
 		member_nickname : String ,		/	닉네임
-		member_profile : file  			/	디폴트 프로필 사진 
 	}
-
 	중복검사가 모두 완료되었을경우에만 진행 안그럴 경우 id PK 로 인해서 오류 발생
 */
 
@@ -23,31 +21,14 @@ const moment = require( 'moment' ) ;
 
 const crypto = require('crypto');
 
-const multer = require('multer');
-const multerS3 = require('multer-s3');
-const aws = require('aws-sdk');
-aws.config.loadFromPath('../config/aws_config.json');	//	server 에서는 2개
-const s3 = new aws.S3();
 
-const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: 'hyunho9304',
-        acl: 'public-read',
-        key: function(req, file, callback) {
-            callback(null, Date.now() + '.' + file.originalname.split('.').pop());
-        }
-    })
-});
-
-router.post('/', upload.single('member_profile'), function(req, res) {
+router.post('/', function(req, res) {
 
 	let member_type = req.body.member_type ;
 	let member_category = req.body.member_category ;
 	let member_ID = req.body.member_ID ;
 	let member_PW = req.body.member_PW ;
 	let member_nickname = req.body.member_nickname ;
-	let member_profile = req.file.location  ;
 
 	let task = [
 
@@ -92,7 +73,7 @@ router.post('/', upload.single('member_profile'), function(req, res) {
 							let cryptopwd = hashed.toString( 'base64' ) ;
 
 							let insertMemberQuery = 'INSERT INTO Member VALUES( ? , ? , ? , ? , ? , ? , ? )' ;
-							let queryArr = [ member_type , member_category , member_ID , cryptopwd , salt , member_nickname , member_profile ] ;
+							let queryArr = [ member_type , member_category , member_ID , cryptopwd , salt , member_nickname , null ] ;
 
 							connection.query( insertMemberQuery , queryArr , function( err , result ) {
 								if(err) {
